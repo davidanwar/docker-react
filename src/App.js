37ef13c95372eb0ecd14c30cc39dbd0fffbce595
david <david@bippo.co.id>
2021-08-11 10:42:08 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React JS
+        </p>
+        <p>
+          Hello React JS
         </p>
         <a
           className="App-link"
